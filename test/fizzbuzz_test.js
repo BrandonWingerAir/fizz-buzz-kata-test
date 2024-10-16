c1c4f5a82b12d22ded4a9c61fe582fc562a549c9
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-16 14:40:09 -0400

    return value 1 test passed

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
 
 function fizzBuzz(value) {
-    ;
+    return '1';
 }
 
-it('call fizzBuzz', function() {
-    fizzBuzz(1);
+it('returns 1 with 1 passed in', function(){
+    var result = fizzBuzz(1);
+    expect(result).to.equal('1');
 });
